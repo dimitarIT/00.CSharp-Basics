7193b73126190d566e8f5bb7db6e408498698c6b
Dimitar Keremedchiev <d.k.keremedchiev@gmail.com>
2014-08-07 12:29:23 +0300

    Loops HW

---
+++ New folder/01. NumbersFrom1ToN/PrintNumbers.cs
@@ -0,0 +1,16 @@
+﻿//Write a program that enters from the console a positive integer n and prints all the numbers from 1 to n, on a single line, separated by a space. 
+
+using System;
+
+class PrintNumbers
+{
+    static void Main()
+    {
+        uint  inputNumber = uint.Parse(Console.ReadLine());
+
+        for (uint i = 1; i <= inputNumber; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
