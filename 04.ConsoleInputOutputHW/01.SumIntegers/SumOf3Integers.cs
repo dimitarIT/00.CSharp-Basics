056273480c1c12e3a8c954220edf4cd647c6663d
Dimitar Keremedchiev <d.k.keremedchiev@gmail.com>
2014-08-07 11:29:45 +0300

    Console Input Output HW

---
+++ 04.ConsoleInputOutputHW/01.SumIntegers/SumOf3Integers.cs
@@ -0,0 +1,17 @@
+﻿//Write a program that reads 3 integer numbers from the console and prints their sum.
+
+using System;
+
+class SumOf3Integers
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter three numbers a; b; c;");
+        float a, b, c, sumOfnumbers;
+        a = float.Parse(Console.ReadLine());
+        b = float.Parse(Console.ReadLine());
+        c = float.Parse(Console.ReadLine());
+        sumOfnumbers = a + b + c;
+        Console.WriteLine("The sum of numbers is: {0}", sumOfnumbers);
+    }
+}
